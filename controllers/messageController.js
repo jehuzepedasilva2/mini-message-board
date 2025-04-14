d279bc086b4e57a67dcd058a337be4133b56f3c7
Jehu Zepeda-Silva <jehuzepeda@yahoo.com>
2025-04-13 21:06:43 -0700

    I think I am done, added back arrows to write a message page, and message page

--- controllers/messageController.js
+++ controllers/messageController.js
@@ -2,7 +2,7 @@ const { getMessage } = require('../db');
 
 function getESJ(req, res) {
   const { id } = req.params;
-  const msg = getMessage(Number(id));
+  const msg = getMessage(id);
   res.render('templates/message', { reqMessage: msg });
 }
 
--- db.js
+++ db.js
@@ -1,12 +1,12 @@
 const messages = [
   {
-    id: 1,
+    id: '1',
     text: "Hi there!", 
     user: "Amando", 
     added: new Date(),
   }, 
   {
-    id: 2,
+    id: '2',
     text: "Hello World!", 
     user: "Charles", 
     added: new Date(),
